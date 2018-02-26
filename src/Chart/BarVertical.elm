8985762794f3d13f6e6b2a347f7cda18f767968c
deciob <decio.battaglia@featurespace.co.uk>
2018-02-26 11:38:10 +0000

    work on types

---
+++ src/Chart/BarVertical.elm
@@ -0,0 +1,9 @@
+module Chart.BarVertical exposing (render)
+
+import Chart.Types as Types
+import Html exposing (..)
+
+
+render : Types.Data -> Types.Config -> Html msg
+render data config =
+    [ text "TODO" ]
--- src/Chart/Types.elm
+++ src/Chart/Types.elm
@@ -1,4 +1,19 @@
-module Chart.Types exposing (DerivedConfig)
+module Chart.Types
+    exposing
+        ( Data
+        , DerivedConfig
+        , PointLinear
+        , PointOrdinal
+        , PointTime
+        , fromData
+        , fromPointLinear
+        , fromPointOrdinal
+        , fromPointTime
+        , toData
+        , toPointLinear
+        , toPointOrdinal
+        , toPointTime
+        )
 
 import Date exposing (Date)
 import Html exposing (..)
@@ -11,9 +26,67 @@ import Visualization.Scale as Scale
         )
 
 
-type Point
-    = Time ( Date, Float )
-    | Ordinal ( String, Float )
+type PointTime
+    = PointTime ( Date, Float )
+
+
+toPointTime : ( Date, Float ) -> PointTime
+toPointTime point =
+    PointTime point
+
+
+fromPointTime : PointTime -> ( Date, Float )
+fromPointTime (PointTime point) =
+    point
+
+
+type PointOrdinal
+    = PointOrdinal ( String, Float )
+
+
+toPointOrdinal : ( String, Float ) -> PointOrdinal
+toPointOrdinal point =
+    PointOrdinal point
+
+
+fromPointOrdinal : PointOrdinal -> ( String, Float )
+fromPointOrdinal (PointOrdinal point) =
+    point
+
+
+type PointLinear
+    = PointLinear ( Float, Float )
+
+
+toPointLinear : ( Float, Float ) -> PointLinear
+toPointLinear point =
+    PointLinear point
+
+
+fromPointLinear : PointLinear -> ( Float, Float )
+fromPointLinear (PointLinear point) =
+    point
+
+
+type alias InternalData point =
+    { cssClass : Maybe String
+    , point : point
+    , title : Maybe String
+    }
+
+
+type Data point
+    = Data (InternalData point)
+
+
+toData : InternalData point -> Data point
+toData data =
+    Data data
+
+
+fromData : Data point -> InternalData point
+fromData (Data point) =
+    point
 
 
 type XScale
@@ -50,10 +123,10 @@ type alias DerivedConfig =
 
 
 deriveHeight : Height -> DerivedConfig -> DerivedConfig
-deriveHeight config height =
+deriveHeight height config =
     { config | width = Maybe.withDefault config.height height }
 
 
 deriveWidth : Width -> DerivedConfig -> DerivedConfig
-deriveWidth config width =
+deriveWidth width config =
     { config | height = Maybe.withDefault config.width width }
